940193acd700a76c717154a60a552060687be330
aperkes <perkes.ammon@gmail.com>
2016-01-14 17:01:09 -0500

    v0.0.2
    Lots of changes, Birds no longer store history, they just keep their
    information and then respond to the history, which is stored and
    applied externally. Working on the run_simulation, it’s almost there.
    Then need to work on strategies.

--- matingsim.py
+++ matingsim.py
@@ -10,21 +10,21 @@ import numpy as np
 from  matplotlib as import pyplot as plt
 
 #Default starting parameters: 
+default_strat_m = 0
+default_strat_f = 1
 default_res_m = 1    # Resource limitation for a male
 default_res_f = 1    # Resource limitations for a female (NOTE: This might be unnecessary also....)
-default_a = 2
-default_k = 6        # Parameter, NOTE: I Might take these out
-default_birds = 5    # Number of birds per trial (assumes symetric trials)
+default_males = 5    # Number of birds per trial (assumes symetric trials)
+default_females = 5    # Number of birds per trial (assumes symetric trials)
 default_turns = 100  # Number of turns per trial
 default_trials = 10  # Number of trials per simulation
 
 #global parameters: 
-global RES_M, RES_F, A, K, BIRDS, TURNS, TRIALS
+global STRAT_M, STRAT_F, RES_M, RES_F, A, K, BIRDS, TURNS, TRIALS
 RES_M = default_res_m
 RES_F = default_res_f
-A = defaul_a
-K = default_k
-BIRDS = default_birds
+N_MALES = default_males
+N_FEMALES = defaul_females
 TURNS = default_turns
 TRIALS = default_trials
 
@@ -32,60 +32,33 @@ TRIALS = default_trials
 #Includes: Resources, investment matrix, reward matrix, functions to alter investment
 #Also includes choice function determining investment
 class Male_bird(object):
-    def __init__(self, num, resouces = 1, strategy = 1):
+    def __init__(self, num, strategy = 0, resources = 1): #Convention: Males have even strategies, females odd. 
         self.num = num
-        self.resources = resources
         self.strategy = strategy
+        self.resources = resources
 ##      Seed self investment, and normalize for resources
-        self.investment = np.random.rand(BIRDS)
-        self.investment = self.invesetment * resources / sum(self.investment)
-        self.reward = np.zeros(BIRDS)
 #   Functions to adjust and get info. 
-    def change_investment(female,amount):
-        self.investment[female] = self.investment[female] + amount  
-    def change_reward(female,amount):
-        self.reward[female] = self.reward[female] + amount
 ### NOTE: This is where males apply strategy
     def respond(history):
-       investment = self.investment
-       reward = self.reward
-       new_investment = strategies.males(self.strategy,history)
-       self.investment = new_investment 
-       return self.investment 
-    def total_reward():
-        return sum(reward))
-    def total_investment():
-        return sum(investment))
+       new_investment = strategies.choose(self.strategy, resources, history)
+       return new_investment 
 
 #Class of female cowbirds: 
 #Includes: resources, response matrix, reward matrix
 #Also includes choice function determining response
 class Female_bird(object):
-    def __init__(self, num, resouces = 1, strategy = 1):
+    def __init__(self, num, strategy = 1, resouces = 1):
         self.num = num
-        self.resources = resources
         self.strategy = strategy
+        self.resources = resources
 ##      Seed self  investment, NOTE that for females this is which males are investing in them, not vice versa
-        self.investment = np.zeros(BIRDS)
 #   Functions to adjust and get info. 
-    def change_investment(male,amount):
-        self.investment[male] = self.investment[male] + amount
-    def change_response(male,amount):
-        self.response[male] = self.response[male] + amount  
-    def change_product(amount):
-        self.product = self.product + amount
     def respond(history):
 ### NOTE: This is where strategy is applied
         investment = self.investment
         resources = self.resources
-        new_response = strategies.females(self.strategy,history)
-        self.response = new_response
-        return self.response
-# Get total production (which is a function of investment I assume) 
-    def total_product(investment):
-        return sum(self.product)
-    def total_investment():
-        return sum(investment))
+        new_response = strategies.choose(self.strategy,self.resources, history)
+        return new_response
 #
 # Function to plot the response curve of a male or female bird (which is contained in their class)
 def plot_response(bird):
@@ -108,14 +81,15 @@ class History(object):
 
 #Object containing turn
 class Turn(object):
-    def __init__(self, n, males, females, last_turn = None):
+    def __init__(self, n, n_males, n_females, last_turn = None):
 ## Initialize based on last turn if desired, otherwise start blank
+        self.n = n
         if last_turn != None:
             self.invest = last_turn.invest
             self.reward = last_turn.reward
         else:
-            self.invest = np.zeros([males,females])
-            self.reward = np.zeros([males,females])
+            self.invest = np.zeros([n_males,n_females])
+            self.reward = np.zeros([n_males,n_females])
     def change_invest(male,female,amount):
         self.invest[male,female] += amount
     def change_reward(male,female,amount):
@@ -125,6 +99,21 @@ class Turn(object):
     def set_reward(male,female,amount):
         self.reward[male,female] = amount
 
+# Object containing all the birds. Cute, eh? 
+class Aviary(object):
+    def __init__(self, n_males, n_females, strat_males, strat_females, res_males, res_females)
+# Initialize some parameters:
+        self.n_males = n_males
+        self.n_females = n_females
+        self.strat_males = strat_males
+        self.strat_females = strat_females
+        self.res_males = res_males
+        self.res_females = res_females
+# Build the male and female lists in the aviary. 
+        self.males = [Male_bird(num, strat_males[num], res_males[num] for num in range(n_males))]
+        self.females = [Female_bird(num, strat_females[num], res_females[num] for num in range(n_females))]
+#NOTE: will I need to give funcitons to change the birds, or will they change automatically? I feel like python is so pointer based that it will be hard to alter it...
+# Actually, it's very easy, in fact, because it's a pointer, it alters the original instance, not merely the aviary object.
 #Function determining reproductve output: 
 #This is not technically important for the simulation, but it determines which strategy is best, which is important
 def female_success(params):
@@ -137,9 +126,163 @@ def male_success(params):
 #Function plotting history and outcome in interesting ways
 def plot_history(history):
 
-def run_simulation(trials = 10,turns = 100,n_males,n_females,strat_males = 1, strat_females = 1, res_males = 1, res_females = 1)
+def run_simulation(trials = TRIALS, turns = TURNS, n_males = N_MALES,n_females = N_FEMALES,strat_males = None, strat_females = None, res_males = None, res_females = None)
+## initialize history
     history = History(turns,n_males,n_females)
+## give values to strats and res if none are given:
+    if strat_males == None: 
+        strat_males = [STRAT_M for n in range(n_males)]
+    if strat_females == None:
+        strat_females = [STRAT_F for n in range(n_females)]
+    if res_males == None:
+        res_males = [RES_M for n in range(n_males)]
+    if res_females == None:
+        res_females = [RES_F for n in range(n_females)]
+# Build an aviary using the given parameters
+    aviary = Aviary(n_males, n_females, strat_males, strat_females, res_males, res_females)
+# Initialize first turn
+    turn = Turn(0,aviary.males,aviary.females)
+    turn.initialize()
+# Run turns for length of simulation
+    for t in range(1,turns):
+        turn = (t, aviary.males,aviary.females,turn)
+        turn.run()
+
+# Mini function to get birds
+def get_birds():
+    print "Default males: " + str(N_MALES)
+    print "Default females " + str(N_FEMALES)
+    choice = raw_input("Would you like to change these? [y/n]")
+    if choice[0].lower() == 'y':
+        try:
+            n_males = raw_input('How many males? ')
+            n_females = raw_input('How many females? ')
+            n_males = int(n_males.strip())
+            n_females = int(n_males.strip())
+        except:
+            print 'bad input, try again:'
+            return get_birds()
+        return(n_females,n_females)
+    else:
+        return (N_MALES,N_FEMALES)
+
+## Mini function to get strategy. Strategy will always be an array of ints matching the number of birds.
+#  If a single number is given, it fills the list (2 -> [2,2,2,2,2]
+#  If it matches teh number of birds, it will just be one to one, otherwise, any leftovers will be filled by default.
+def get_strategy(n_males = N_MALES, n_females = N_FEMALES):
+    print "Default strategy: 1"
+    print "Enter either a single strategy (2) or multiple strategies, seperated by commas (2,3,4,5)"
+    print "If there are more birds than strategies, the rest will be filled by default."
+    print "If there are more strategies than birds, the last will be cut off."
+    print "...."
+    print "N Males: " + str(n_males)
+    print "What strategies would you like to use?"
+# get input: 
+    m_strat = raw_input()
+# clean it up and put it in the right form.
+    m_strat = m_strat.replace(' ','') 
+    m_strat = m_strat.split(',')
+    m_strat = map(int, m_strat)
+# If a single number was given, it fills the list
+    if len(m_strat) == 1:
+        m_strat = m_strat * n_males
+# If too few numbers were given, the rest are filled by default
+    elif len(m_strat) < n_males:
+        for i in range(len(m_strat),n_males):
+            m_strat[i] = 1
+# if the right number of numbers is given, well done
+    elif len(m_strat) == n_males:
+        pass
+# IF too many numbers are given, the last ones are chopped off.
+    elif len(m_strat) > n_males:
+        del m_strat[n_males:]
+    print "...."
+    print "N Females: " + str(n_females)
+    print "What strategies would you like to use?"
+# get input: 
+    f_strat = raw_input()
+# clean it up and put it in the right form.
+    f_strat = f_strat.replace(' ','') 
+    f_strat = f_strat.split(',')
+    f_strat = map(int, f_strat)
+    if len(f_strat) == 1:
+        f_strat = f_strat * n_females
+    elif len(f_strat) < n_females:
+        for i in range(len(f_strat),n_females):
+            f_strat[i] = 1
+    elif len(f_strat) == n_females:
+        pass
+    elif len(f_strat) > n_females:
+        del f_strat[n_females:]
+    return m_strat,f_strat
+
+def get_resources(n_males = N_MALES, n_females = N_FEMALES):
+    print "This will work just like strategy."
+    print "Default males resources: " + str(RES_M)
+    print 'What resources would you like the males to have'
+    m_res = raw_input()
+    m_res = m_res.replace(' ','')
+    m_res = m_res.split(',')
+    m_res = map(int, m_res)
+    if len(m_res) == 1:
+        m_res = * n_males
+    elif len(m_res) < n_males:
+        for i in range(len(m_res), n_males):
+            m_res[i] = 1
+    elif len(m_res) == n_males:
+        pass
+    elif len(m_res) > n_males:
+        del m_res[n_males:]
+    print "...."
+    print "N Females: " + str(n_females)
+    print 'What resources would you like the females to have?'
+# get input: 
+    f_res = raw_input()
+# clean it up and put it in the right form.
+    f_res = f_res.replace(' ','') 
+    f_res = f_res.split(',')
+    f_res = map(int, f_res)
+    if len(f_res) == 1:
+        f_res = f_res * n_females
+    elif len(f_res) < n_females:
+        for i in range(len(f_res),n_females):
+            f_res[i] = 1
+    elif len(f_res) == n_females:
+        pass
+    elif len(f_res) > n_females:
+        del f_res[n_females:]
+    return m_res,f_res
+
+# function to set up a run custon simulations
+def build_simulation():
+    print "This will help you build a simulation. Enter all values as integers (i.e., 11)"
+    turns = raw_input("How turns per trial would you like? ")
+    turns = int(turns.strip())
+    trials = raw_input("How many trials in the simulation would you like? ")
+    trials = int(trials.strip())
+    n_males,n_females = get_birds()
+    stat_males,strat_females = get_strategy()
+    res_males,res_females = get_res()
+    run_simulation(trials, turns, n_males, n_females, strat_males, strat_females, res_males, res_females)
+    
+# List of Menu options
+def print_menu():
+    print "[0] - Run Default Simulation"    
+    print "[1] - Run Default Single Trial"
+    print "[2] - Run Custom Simulation (set parameters)"
+    print "[9] - Quit"
+
 #Menu allowing you to set the parameters (based on sys.argv)
 def menu():
-
+    while choice != 9:
+        print_menu()
+        choice = raw_input("Select option (enter a number)")
+        if choice == 0:
+            run_simulation()
+        elif choice == 1:
+            run_simulation(trials = 1)
+        elif choice == 2:
+            build_simulation()
+        elif choice == 9
+            print "How about a nice game of chess?"
     return choice 
