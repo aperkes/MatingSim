ccd8fcbfe6a39dca622f6b12f3a71084c1e200ce
aperkes <perkes.ammon@gmail.com>
2018-02-17 20:03:40 -0500

    v0.5.2 - Are you asking for a challenge?
    - Basically, it all works. Monogamy occurs as a function of the decay
    constant and the bump constant. So far alpha, iota, and beta don’t seem
    to have a huge impact… need to look into that further.
    - Competition as a mechanic is fully implemented*
    *That said, the strategy for both males and females is to run away from
    any competition (up to half of their resources at the moment). The cost
    function is set such that the cost of being competed against is
    actually fairly low, that could change, but it would mean I need to
    think a little bit about what it means when your investment becomes
    negative (death? roll over?), and whether it makes sense for
    males/females to start out in random competition with each other (maybe
    it doesn’t)

--- SimStrategies.py
+++ SimStrategies.py
@@ -24,18 +24,22 @@ KAPPA = 1.0
 
 def choose(strategy, resources, history, num, alpha = ALPHA, kappa = KAPPA):
 ## Male strategies
-    if strategy == 'M1':
-        return m_new_strategy(resources, history, num) 
-    elif strategy == 'M0':
-        return m_classic_strategy(resources, history, num)
-    
+    if strategy == 'M0':
+        return m_classic_flight(resources, history, num) 
+    elif strategy == 'M1':
+        return m_new_strategy
+    elif strategy == 'M2':
+        return m_classic_strategy(resources, history, num)    
+
 ## Female strategies (by convention, odd)
+    elif strategy == 'F0':
+        return f_classic_flight(resources, history, num)
     elif strategy == 'F1':
         return f_new_strategy(resources, history, num)
-    elif strategy == 'F0':
+    elif strategy == 'F2':
         return f_classic_strategy(resources, history, num)
     else:
-        print "No strategy found, quitting"
+        print "No strategy: " + str(strategy) + " found, quitting"
         sys.exit()
 
 ## Some basic functions used in various strategies. NOTE: due to python's structure, changing output within a function will change the output itself, since it's merely a pointer towards the object itself. With that in mind, all the returns are sort of more for good habit, just remember that you can't make an identity and then change one of the lists (that goes for arrays and lists)
@@ -81,6 +85,71 @@ def subtract_output(output,amount,sink):
 
 # Inputs: Resources, history, num of male
 # Output: A New investment matrix, edited for that one male. 
+
+def m_classic_flight(resources, history, num):
+    shift_scaler = .01
+    n_males = history.n_males
+    
+    current_turn = history.current_turn
+    current_invest = history.invest_matrix[current_turn]
+    
+    ## Choose how to compete
+    compete = compete_flee_m(resources,history,num)
+
+    ## Balance Resources
+    comp_res = sum(np.abs(compete))
+    comp_cost = get_cost_m(history, num)
+    mate_resources = resources - comp_cost - comp_res
+    
+    ## Figure out female investments
+    my_previous_invest = np.zeros(np.shape(history.reward_matrix[current_turn - 1,num,:]))
+    my_previous_reward = np.zeros(np.shape(history.reward_matrix[current_turn - 1,num,:]))
+
+    my_previous_invest[:] = history.invest_matrix[current_turn - 1,num,:]
+    my_previous_reward[:] = history.reward_matrix[current_turn - 1,num,:]
+    
+    # Temporarily force male investment/reward to 0 so that it doesn't cause problems
+    my_previous_invest[:n_males] = 0
+    my_previous_reward[:n_males] = 0
+    
+    my_current_invest = np.zeros(np.shape(my_previous_invest))
+    
+    profit = my_previous_reward[:] / (my_previous_invest[:] + .0001)
+
+    avg_profit = profit[my_previous_invest > 0.0].mean()
+    
+    shift_matrix = np.zeros(np.shape(profit))    
+    shift_matrix[profit > avg_profit] = shift_scaler
+    shift_matrix[profit < avg_profit] = -shift_scaler
+
+    my_current_invest = my_previous_invest + shift_matrix
+    my_current_invest[my_current_invest < 0] = 0
+    
+    my_current_invest = my_current_invest / (sum(abs(my_current_invest)) +.0001) * mate_resources ## this normalizes it to resources...
+    my_current_invest[:n_males] = compete[:]
+    current_invest[num,:] = my_current_invest    
+    return current_invest
+
+def get_cost_m(history,num):
+    a = 1.0
+    current_turn = history.current_turn
+    previous_invest = history.invest_matrix[current_turn - 1]
+    n_males = history.n_males
+    cost = [0] * n_males
+    for m in range(n_males):
+        cost[m] = (max(previous_invest[m,num],0) + min(previous_invest[num,m],0)) * history.adjacency_matrix[current_turn,num,m] ** a 
+    return sum(cost)
+
+def compete_flee_m(resources,history,num):
+    compete = [0] * history.n_males
+    previous_invest = history.invest_matrix[history.current_turn - 1]
+    # Run from any competition
+    for m in range(history.n_males):
+        compete[m] = -1 * max(0,previous_invest[m,num])    
+    # Make sure you're not going into negatives
+    if sum(compete) > (resources / 2.0):
+        compete = compete / sum(compete) * (resources / 2.0)
+    return compete
 
 def m_new_strategy(resources, history, num):
     shift_scaler = .1
@@ -202,8 +271,13 @@ def f_classic_strategy(resources, history, num):
     current_invest = np.empty_like(previous_invest)
     current_invest[:] = previous_invest[:]
     
-    my_previous_reward = history.reward_matrix[current_turn - 1,history.n_males + num,:]
-    my_previous_invest = history.invest_matrix[current_turn - 1,history.n_males + num,:]
+    my_previous_reward = np.zeros(np.shape(history.reward_matrix[current_turn - 1,history.n_males + num,:]))
+    my_previous_invest = np.zeros(np.shape(my_previous_reward))
+
+    my_previous_reward[:] = np.zeros(np.shape(history.reward_matrix[current_turn - 1,history.n_males + num,:]))
+    my_previous_invest[:] = history.invest_matrix[current_turn - 1,history.n_males + num,:]
+    my_previous_invest[n_males:] = 0
+                                  
     my_current_invest = np.zeros(np.shape(my_previous_invest))
     
     profit = my_previous_reward[:] / (my_previous_invest[:] + .0001)
@@ -222,3 +296,74 @@ def f_classic_strategy(resources, history, num):
     current_invest[history.n_males + num,:] = my_current_invest    
     return current_invest
                 
+def f_classic_flight(resources, history, num):
+    shift_scaler = .01
+    n_males = history.n_males
+    current_turn = history.current_turn
+    
+    current_invest = history.invest_matrix[current_turn]
+    
+    # Make Competition Decisions
+    compete = compete_flee_f(resources, history, num)
+    # Balance Resources
+    comp_res = sum(np.abs(compete))
+    comp_cost = get_cost_f(history, num)
+    mate_resources = resources - comp_cost - comp_res
+    
+    ## Mate with males
+    my_previous_reward = np.zeros(np.shape(history.reward_matrix[current_turn - 1,history.n_males + num,:]))
+    my_previous_invest = np.zeros(np.shape(my_previous_reward))
+
+    my_previous_reward[:] = history.reward_matrix[current_turn - 1,history.n_males + num,:]
+    my_previous_invest[:] = history.invest_matrix[current_turn - 1,history.n_males + num,:]
+    
+    ## Force old female investment to 0 to avoid skewing avg
+    my_previous_invest[n_males:] = 0
+    my_previous_reward[n_males:] = 0                             
+    
+    my_current_invest = np.zeros(np.shape(my_previous_invest))
+    
+    profit = my_previous_reward[:] / (my_previous_invest[:] + .0001)
+
+    avg_profit = profit[my_previous_invest > 0.0].mean()
+    
+    shift_matrix = np.zeros(np.shape(profit))    
+    shift_matrix[profit > avg_profit] = shift_scaler
+    #from IPython.core.debugger import Tracer; Tracer()()
+    shift_matrix[profit < avg_profit] = -shift_scaler
+
+    my_current_invest = my_previous_invest + shift_matrix
+    my_current_invest[my_current_invest < 0] = 0
+
+    
+    my_current_invest = my_current_invest / (sum(abs(my_current_invest)) +.0001) * mate_resources 
+    my_current_invest[history.n_males:] = compete
+        
+    current_invest[history.n_males + num,:] = my_current_invest   
+    return current_invest
+
+def get_cost_f(history,num):
+    a = 1.0
+    current_turn = history.current_turn
+    previous_invest = history.invest_matrix[current_turn - 1]
+    n_males = history.n_males
+    n_females = history.n_females
+    cost = [0] * n_females
+    j = num + history.n_males
+    for f in range(n_females):
+        i = f + n_males
+        cost[f] = (max(previous_invest[i,j],0) + min(previous_invest[j,i],0)) * history.adjacency_matrix[current_turn,j,i] ** a 
+    return sum(cost)
+
+def compete_flee_f(resources,history,num):
+    compete = [0] * history.n_females
+    previous_invest = history.invest_matrix[history.current_turn - 1]
+    # Run from any competition
+    j = num + history.n_males
+    for f in range(history.n_females):
+        i = f + history.n_males
+        compete[f] = -1 * max(0,previous_invest[i,j])    
+    # Make sure you're not going into negatives
+    if sum(compete) > (resources / 2.0):
+        compete = compete / sum(compete) * (resources / 2.0)
+    return compete
